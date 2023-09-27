0a236fc945286a365afba278d7efcc83c6965744
Fauzan Shidqi <fauzanshidqi21@gmail.com>
2023-09-27 23:30:40 +0700

    add UserController.php

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreUserRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $users = DB::table('users')
+            ->when($request->input('name'), function($query, $name){
+                return $query->where('name', 'like', '%'.$name.'%');
+            })
+            ->select('id', 'name', 'email', 'phone', DB::raw('DATE_FORMAT(created_at, "%D %M %Y") as created_at '))
+            ->paginate(10);
+        return view('pages.users.index', compact('users'));
+    }
+
+    public function create(){
+        return view('pages.users.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUserRequest $request)
+    {
+        User::create([
+            'name' => $request['name'],
+            'email' => $request['email'],
+            'password' => Hash::make($request['password']),
+            'roles' => $request['roles'],
+            'phone' => $request['phone'],
+            'address' => $request['address'],            
+        ]);
+
+        return redirect(route('user.index'))->with('success', 'New User Successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
